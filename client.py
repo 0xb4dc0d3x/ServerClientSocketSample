2196c2aa96eceb9dcfd7067ba2f58b8da18d4736
Zero Ex <0xb4dc0d3x@gmail.com>
2023-02-12 02:30:00 +0330

    Initial commit, ye yey

---
+++ client.py
@@ -0,0 +1,26 @@
+import socket
+
+
+def client_program():
+    # host = (input("Enter server hostname(IP) -> "))  # get server IP address
+    host = socket.gethostname()  # this hostname works on local machine
+    port = int(input("Enter server port number -> "))  # socket server port number
+
+    client_socket = socket.socket()  # instantiate
+    client_socket.connect((host, port))  # connect to the server
+
+    message = input(" -> ")  # take input
+
+    while message.lower().strip() != 'bye':
+        client_socket.send(message.encode())  # send message
+        data = client_socket.recv(1024).decode()  # receive response
+
+        print('Received from server: ' + data)  # show in terminal
+
+        message = input(" -> ")  # again take input
+
+    client_socket.close()  # close the connection
+
+
+if __name__ == '__main__':
+    client_program()
